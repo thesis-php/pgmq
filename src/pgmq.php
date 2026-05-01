5c22e599996fd149027c9698e11adc945043565d
Valentin Udaltsov <udaltsov.valentin@gmail.com>
2026-05-01 03:09:02 +0300

    Rename queue.php to queue.fn.php

--- src/pgmq.php
+++ src/pgmq.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 namespace Thesis\Pgmq;
 
 require_once __DIR__ . '/extension.php';
-require_once __DIR__ . '/queue.php';
+require_once __DIR__ . '/queue.fn.php';
 require_once __DIR__ . '/topic.php';
 require_once __DIR__ . '/fifo.php';
